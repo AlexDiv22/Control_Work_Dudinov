c96e9f8f63ecc5430e1c3e5a7612122326c74335
Alexey_Dudinov <dudinov-aleksey@mail.ru>
2022-09-18 02:38:55 +0400

    Добавил программу, решающую задачу

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿string [] array = {"Seminar", "2022", "22", "09", "18", "Work", "Wolwes", "abc", "park", "hi", "1hj"};
+Console.WriteLine($"Исходный массив [{String.Join("; ", array)}]");
+int size = array.Length;
+string [] array_cut = new string [size];
+for (int index = 0; index < size; index++)
+{
+    if (array[index].Length <= 3)
+        {
+            array_cut[index]=array[index];
+        }
+}
+Console.WriteLine($"Массив из 3х и менее символов - [{String.Join("; ", array_cut)}]");
